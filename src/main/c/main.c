5aec93094d9a735db3f7c7177657319c1c755d61
Christian Seibold <krixano@outlook.com>
2016-12-27 00:37:22 -0600

    Small Changes(could be breaking changes)

--- src/main/c/main.c
+++ src/main/c/main.c
@@ -1,20 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
+
+#ifdef _UNIX
 #include <unistd.h>
 #include <dirent.h>
-#include <errno.h>
+#define BOOL int
+#define TRUE 1
+#define FALSE 0
+#endif
+
+#ifdef _WIN32
+#include <windows.h>
+#include <Shlwapi.h>
+#endif
+
+int compile(const char files[1000]) {
+	//return system("gcc *.c -o main");
+	char path[1024];
+	strcpy(path, "gcc ");
+	strcat(path, files);
+	strcat(path, " -o main");
+#ifdef _UNIX
+	return system(path);
+#endif
+#ifdef _WIN32
+#endif	
+}
+
+int fileExists(const char *unixfilename, const char *windowsfilepath) {
+#ifdef _UNIX
+	return access(unixfilename, F_OK) == 0;
+#endif
+#ifdef _WIN32
+	return PathFileExists(windowsfilepath);
+#endif
+}
+
+int dirExists() {
+
+}
 
 int main() {
 	int status = 0;
-	if (access("main.c", F_OK) == 0) {
-		status = system("gcc *.c -o main");
+	if (fileExists("main.c", "main.c") == 0) {
+		status = compile("*.c");
 	} else {
 		//printf("Looking in src/main/c/\n");
 		// Check if src/main/c/ directory actually exists, error if it doesn't
 		DIR *dir = opendir("./src/main/c/");
 		if (dir) {
 			closedir(dir);
-			status = system("gcc src/main/c/*.c -o main");
+			status = compile("src/main/c/*.c");
 		} else if (ENOENT == errno) {
 			closedir(dir);
 			printf("[Error] src/main/c/ directory does not exist!\n");
